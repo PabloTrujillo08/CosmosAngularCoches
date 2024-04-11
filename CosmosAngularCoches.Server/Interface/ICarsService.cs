5b8679718c73be15169bf5c191f442d7212975f7
Pablo Trujillo <Pablo.TrujilloAvendano@tajamar365.com>
2024-04-11 19:40:27 +0200

    interface created

---
+++ CosmosAngularCoches.Server/Interface/ICarsService.cs
@@ -0,0 +1,13 @@
+﻿using CosmosAngularCoches.Server.Models;
+
+namespace CosmosAngularCoches.Server.Interface
+{
+    public interface ICarsService
+    {
+        Task<IEnumerable<Cars>> GetCarsAsync(string query);
+        Task<Cars> GetCarAsync(string id);
+        Task<Cars> AddCarAsync(Cars car);
+        Task<Cars> UpdateCarAsync(Cars car);
+        Task DeleteCarAsync(string id);
+    }
+}
---
+++ CosmosAngularCoches.Server/Services/CarsService.cs
@@ -0,0 +1,34 @@
+﻿using CosmosAngularCoches.Server.Interface;
+using CosmosAngularCoches.Server.Models;
+
+namespace CosmosAngularCoches.Server.Services
+{
+    public class CarsService : ICarsService
+    {
+
+        public Task<Cars> AddCarAsync(Cars car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCarAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Cars> GetCarAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Cars>> GetCarsAsync(string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Cars> UpdateCarAsync(Cars car)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
