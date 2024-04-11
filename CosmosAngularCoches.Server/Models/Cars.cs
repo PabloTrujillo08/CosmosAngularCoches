99235615b99c7990aa95cf1d4332e9954958a08e
Pablo Trujillo <Pablo.TrujilloAvendano@tajamar365.com>
2024-04-11 19:32:51 +0200

    model created

---
+++ CosmosAngularCoches.Server/Models/Cars.cs
@@ -0,0 +1,9 @@
+﻿namespace CosmosAngularCoches.Server.Models
+{
+    public class Cars
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Make{ get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+    }
+}
