fca9af47c17182929c04548b48252c3e2b23fa4d
Pablo Trujillo <Pablo.TrujilloAvendano@tajamar365.com>
2024-04-11 19:59:02 +0200

    program.cs configured

--- CosmosAngularCoches.Server/Services/CarsService.cs
+++ CosmosAngularCoches.Server/Services/CarsService.cs
@@ -1,10 +1,21 @@
 ﻿using CosmosAngularCoches.Server.Interface;
 using CosmosAngularCoches.Server.Models;
+using Microsoft.Azure.Cosmos;
+using System.ComponentModel;
 
 namespace CosmosAngularCoches.Server.Services
 {
     public class CarsService : ICarsService
     {
+
+        private readonly Microsoft.Azure.Cosmos.Container _container;
+
+
+        public CarsService(CosmosClient dbClient, string databaseName, string containerName)
+        {
+            _container = dbClient.GetContainer(databaseName, containerName);
+        }
+
 
         public Task<Cars> AddCarAsync(Cars car)
         {
